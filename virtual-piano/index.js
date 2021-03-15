b4b0e27f2f9826fb6514baa50ca1582b5e093bfe
Oxana Danilova <oxanadanilova11@gmail.com>
2021-03-15 19:46:23 +0100

    feat: handle piano key clicking

--- virtual-piano/index.js
+++ virtual-piano/index.js
@@ -1,10 +1,29 @@
 const app = () => {
     const piano = document.getElementsByClassName('piano')[0];
+    const pianoKey = document.getElementsByClassName('piano-key');
+
+   
+        
+    const playAudio = (src) => {
+        const audio = new Audio();
+        audio.currentTime = 0;
+        audio.src = src;
+        audio.play();
+      }
+
     const handlePiano = (event) => {
-        console.log(event);
-    }
-    piano.addEventListener('click', handlePiano);
-    
+        let target = event.target;     
+        if (target.classList.contains('piano-key')) {
+          console.log(target.dataset.note);
+          let fileName = target.dataset.note;
+          let src = `./assets/audio/${fileName}.mp3`;
+          console.log(src);
+          playAudio(src);
+            }
+        }
+
+      
+    piano.addEventListener('click', handlePiano);   
 }
 
 app();
